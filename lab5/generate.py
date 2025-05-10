6aa8de384d400ab24cddf8ee511c5a81be7af9c0
Ranik23 <antonfedorov190@yandex.ru>
2025-05-10 12:50:11 +0300

    fix

--- lab5/generate.py
+++ lab5/generate.py
@@ -1,7 +1,7 @@
 from PIL import Image, ImageDraw, ImageFont
 import os
 
-output_folder = "images"
+output_folder = "lab5/images"
 if not os.path.exists(output_folder):
     os.makedirs(output_folder)
 
@@ -10,13 +10,13 @@ letters = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
 font = ImageFont.truetype("Times New Roman.ttf", 52)
 
 for letter in letters:
-    img = Image.new('L', (100, 100), color=255)  # белый фон
+    img = Image.new('L', (90, 80), color=255)  # белый фон
     draw = ImageDraw.Draw(img)
 
     # Вычисление размеров текста
     text_bbox = draw.textbbox((0, 0), letter, font=font)
     text_width, text_height = text_bbox[2] - text_bbox[0], text_bbox[3] - text_bbox[1]
-    position = ((100 - text_width) // 2, (100 - text_height) // 2)
+    position = ((90 - text_width) // 2, (80 - text_height) // 2)
 
     draw.text(position, letter, fill=0, font=font)  # черный текст
 
